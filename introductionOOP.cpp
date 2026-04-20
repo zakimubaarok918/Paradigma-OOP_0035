88056653d45ee32340d7f6d3aa4a555d18e01332
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-04-20 10:49:39 +0700

    Implement Mahasiswa class and main function to display student data

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -1,2 +1,25 @@
 #include <iostream>
 using namespace std;
+
+class Mahasiswa{
+
+public :
+    int nim;
+    string nama;
+    float nilai;
+
+    void printdata{
+        cout << "NIM : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
+    }
+}; //batas class
+
+int main(){
+    Mahasiswa mhs1;
+    mhs1.nim = 12345;
+    mhs1.nama = "Zaki";
+    mhs1.nilai = 90.5;
+
+    mhs1.printdata();
+}
